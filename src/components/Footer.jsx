ecc983d17b4c525e78b129b0d944222c44f23f54
Itai Hammer <61294759+ItaiHammer@users.noreply.github.com>
2020-11-09 21:23:07 -0800

    Fixed Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,11 @@ import './Footer.css';
 export default function Footer () {
     return (<div id="footer" >
 
-        <img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/code-512.png" className="made-icon" />
-        <p className="footerp" >With ❤ By</p><h1 className="footerh1" >Itai Hammer</h1>
+        <img src="https://cdn2.iconfinder.com/data/icons/font-awesome/1792/code-512.png" className="footer-icons" />
+        <p className="footerp" >With</p>
+        <img src="https://cdn2.iconfinder.com/data/icons/pittogrammi/142/80-512.png" className="footer-icons" />
+        <p className="footerp" >By</p>
+        <h1 className="footerh1" >Itai Hammer</h1>
         <a href="https://github.com/ItaiHammer/website" target="blank" className="footera" >GitHub Page</a>
     </div>);
 }
